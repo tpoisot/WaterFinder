79b58cf9d1c29bfc884d6931cbfe15fa501f08a4
Timothée Poisot <tim@poisotlab.io>
2017-03-03 21:02:56 -0500

    update the main code to detect areas in another image

--- findWater.jl
+++ findWater.jl
@@ -4,7 +4,7 @@ using StatsBase;
 include("./utils.jl");
 
 img = load("./img/t1.png");
-samples = imgsplit(img);
+samples = imgsplit(img, gridsize=40);
 
 # Then we extract the features -- mean and median of every primary color
 features = getfeatures.(samples);
@@ -12,7 +12,7 @@ features = getfeatures.(samples);
 # We will get some infos for the calibration dataset
 training_candidates = sample(eachindex(samples), length(eachindex(samples)), replace=false);
 
-traing_size = 20;
+traing_size = 40;
 labels = Array{String, 1}(traing_size);
 positions = Array{Int64, 1}(traing_size);
 labelcounter = 1;
@@ -35,12 +35,18 @@ while labelcounter <= traing_size
   end
 end
 
-training_set = features[positions]
+training_set = features[positions];
 
-# Take a guess at every element
-guesses = Array{String, 2}(size(samples));
+# Take a guess at every element in the other image
+
+img2 = load("./img/test.png");
+samples2 = imgsplit(img2, gridsize=25);
+# Then we extract the features -- mean and median of every primary color
+features2 = getfeatures.(samples2);
+
+guesses = Array{String, 2}(size(samples2));
 for i in eachindex(guesses)
-  guesses[i] = classify(i, features, training_set, labels, k=4)
+  guesses[i] = classify(i, features2, training_set, labels, k=4)
 end
 
 for i in 1:size(guesses, 1)
